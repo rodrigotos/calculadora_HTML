465b5058dbf76fc9b3a53f88ae8dcd15f3535790
Rodrigo Torres <rodrigotos3@hotmail.com>
2016-10-03 21:38:46 -0500

    Creación

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  var output = document.getElementById("result");
+  var controlpad = document.getElementById("controlpad");
+});
+
+calculator = {
+  op1: null,
+  op2: null,
+  operator: null,
+  check_input: function(){
+    var input = document.getElementById("input"); // input del ususario
+    var input_text = input.textcontent; // texto
+  }
+}
